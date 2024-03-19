89312ed3fc8832738fd1141c59c2560dff680c4e
nicodev711 <124818423+nicodev711@users.noreply.github.com>
2024-03-19 10:35:06 +0000

    update routes

---
+++ src/components/userHomePage/LayoutUserHP.jsx
@@ -0,0 +1,14 @@
+const Layout = () => {
+    return (
+        <div className={"md:container mx-auto"}>
+            <div className={"columns-3 bg-amber-600"}>
+                <p>Col 1</p>
+            </div>
+            <div className={"columns-9 bg-green-600"}>
+                <p>Col 1</p>
+            </div>
+        </div>
+    )
+};
+
+export default Layout;
